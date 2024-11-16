1fde9b739c2bfd4aa2f84042c08fa094227d7e0c
Robert Ruta <robertruta50@gmail.com>
2024-11-16 09:57:53 +0000

    Added SerializeField attribute to time cost variable so that it is editable in the Unity editor (debug purposes).

--- Assets/Scripts/ListComponents/TasksView/Task.cs
+++ Assets/Scripts/ListComponents/TasksView/Task.cs
@@ -1,6 +1,8 @@
+using UnityEngine;
+
 public class Task : BaseListItem
 {
-    uint _timeCost; // seconds
+    [SerializeField] uint _timeCost; // seconds
     public uint TimeCost { get { return _timeCost; } set { _timeCost = value; } } 
     TaskTier _taskTier;
     public TaskTier TaskTier { get { return _taskTier; } set { _taskTier = value; } } 
