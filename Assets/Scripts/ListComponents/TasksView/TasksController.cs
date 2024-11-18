eb7a2ad9cd3c2bede27be082c0332956e3157a27
Robert Ruta <robertruta50@gmail.com>
2024-11-18 18:08:45 +0000

    Added some provisional currency and xp producing code to the Task Complete List Item function.

--- Assets/Scripts/ListComponents/TasksView/TasksController.cs
+++ Assets/Scripts/ListComponents/TasksView/TasksController.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Xml.XPath;
 using UnityEngine;
 
 // This class
 public class TasksController : ListController<Task>
 {
     TasksUIController TasksUIController;
+
 
     void Awake()
     {
@@ -19,10 +22,37 @@ public class TasksController : ListController<Task>
         // If activate chosen, create focus session based on task data
     }
 
+
     public override void CompleteListItem()
     {
+        // Provisional solution.
+        // TODO: Come up with a better way of converting completed tasks to coins and xp
         // Reward with coins and xp
+        switch (selectedListItem.TaskTier)
+        {
+            case TaskTier.Easy:
+                // Logic for easy tasks
+                gameManager.coins += 2;
+                gameManager.xp += 100; 
+                break;
+
+            case TaskTier.Medium:
+                // Logic for medium tasks
+                gameManager.coins += 5;
+                gameManager.xp += 200;
+                break;
+
+            case TaskTier.Hard:
+                // Logic for hard tasks
+                gameManager.coins += 10;
+                gameManager.xp += 500;
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException(nameof(selectedListItem.TaskTier), "Unexpected TaskTier value.");
+        }
     }
+
 
     public override void SelectListItem()
     {
