93015e084cd96233bde52a9336c8f34bdaff5ccd
Robert Ruta <robertruta50@gmail.com>
2024-12-02 22:44:16 +0000

    Starting to work on the NewListItemUIController.

--- Assets/NewListItemUIController.cs
+++ Assets/NewListItemUIController.cs
@@ -1,10 +1,12 @@
+using TMPro;
 using UnityEngine;
 
 
 
-public class NewListItemUIController : MonoBehaviour
+public class NewListItemPanelUIController : MonoBehaviour
 {
-
+    public TMP_InputField itemTitle;
+    public TMP_InputField itemDescription;
     // Get all the input fields (text or otherwise) from the NewListItemPanel GameObject
 
     // Expose methods that validate whether user input in input fields is valid
