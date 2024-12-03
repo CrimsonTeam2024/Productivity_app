a7f9b0e4703a1f4292476759f8d11d8de9d795a8
Robert Ruta <robertruta50@gmail.com>
2024-12-03 17:40:45 +0000

    Some cleanup. Also bound the correct function to the cancel button.

--- Assets/NewListItemUIController.cs
+++ Assets/NewListItemUIController.cs
@@ -11,18 +11,6 @@ public abstract class NewListItemPanelUIController<T> : MonoBehaviour where T : 
     public TMP_InputField itemDescription;
     public TMP_Dropdown itemTier;
     public Button saveButton, cancelButton;
-    private void Start()
-    {
-        // Assign the SaveButtonOnClick method to the saveButton's onClick event
-        // saveButton.onClick.AddListener(SaveButtonOnClick);
-    }
-
-    /// <summary>
-    /// This method is called when the save button is clicked.
-    /// </summary>
-    // public abstract void SaveButtonOnClick();
-
-    // protected abstract void HandleSave();
 
     public abstract T GetListItemFromUI();
 }
--- Assets/NewTaskUIController.cs
+++ Assets/NewTaskUIController.cs
@@ -11,10 +11,13 @@ public class NewTaskUIController : NewListItemPanelUIController<TaskData>
     public Slider timeCostSlider;
     SliderController sliderController;
 
+
     void Awake()
     {
         sliderController = timeCostSlider.GetComponent<SliderController>();
     }
+
+
     public override TaskData GetListItemFromUI()
     {
         TaskData taskData = new TaskData {
@@ -26,40 +29,6 @@ public class NewTaskUIController : NewListItemPanelUIController<TaskData>
         
         return taskData;
     }
-
-
-    // public override void SaveButtonOnClick()
-    // {
-    //     TaskItemData taskData = new TaskItemData() {
-    //         title = itemTitle.text,
-    //         description = itemDescription.text,
-    //         tier = (TaskTier)itemTier.value,
-    //         // timeCost = timeCostSlider.value;
-    //     };
-
-    //     // Validate inputs (you can customize this part as needed)
-    //     if (string.IsNullOrWhiteSpace(taskData.title))
-    //     {
-    //         Debug.LogWarning("Title cannot be empty.");
-    //         return;
-    //     }
-
-    //     if (string.IsNullOrWhiteSpace(taskData.description))
-    //     {
-    //         Debug.LogWarning("Description cannot be empty.");
-    //         return;
-    //     }
-
-    //     // Call a method to handle the data (to be implemented in the subclass)
-    //     HandleSave();
-    // }
-
-    // protected void HandleSave()
-    // {
-    //     tasksController.CreateNewListItem(TaskItemData taskData);
-    // }
-
-    // Get all the input fields (text or otherwise) from the NewListItemPanel GameObject
 
     // Expose methods that validate whether user input in input fields is valid
 
