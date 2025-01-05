98d4b3a7ba6f0ab10e5d580e3ec707209222e7a9
Robert Ruta <robertruta50@gmail.com>
2025-01-05 15:30:27 +0400

    Connected task name and description data to focus.

--- Assets/Scripts/FocusModule/FocusController.cs
+++ Assets/Scripts/FocusModule/FocusController.cs
@@ -17,9 +17,9 @@ public class FocusController : MonoBehaviour
     public void InitFocusSession(Task taskToActivate) // TODO: Connect ListController to this
     {
         ShowFocusView();
+        focusUIController.UpdateFocusViewData(taskToActivate);
         focusedTask = taskToActivate;
     }
-
 
     public void StartFocusTimer() // TODO: Connect ListController to this
     {
@@ -27,13 +27,13 @@ public class FocusController : MonoBehaviour
         focusTime = new Timer(activatedTask.TimeCost);
         focusTime.OnTimerEnd += EndFocusTimer; // Subscribes the EndFocusTimer method to the OnTimerEnd Event
         
-        focusUIController.FakeShowFocusTimer(focusTime);
+        focusUIController.ShowFocusTimer(focusTime);
         
         StartCoroutine(focusTime.StartClock(activatedTask));
 
         // TODO: Handle updates to Village System
     }
-    
+
 
     public void EndFocusTimer(Task completedTask)
     {
