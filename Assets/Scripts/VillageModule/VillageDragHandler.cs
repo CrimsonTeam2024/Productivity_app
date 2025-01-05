294d444434d1882607f4d0f8dc42e73941c2d0ee
RobertRuta <77187208+RobertRuta@users.noreply.github.com>
2025-01-05 21:31:10 +0400

    Village animations/rob (#47)
    * Added an inertia to the drag.
    
    * Added inflation animation utility.
    
    * Forced commit of random bullshit that no one knows why it is being forced on us.
    
    * Added some frame rate normalisation to the drag speed.

--- Assets/Scripts/VillageModule/VillageDragHandler.cs
+++ Assets/Scripts/VillageModule/VillageDragHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,7 +10,13 @@ public class VillageDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler
     private RectTransform rectTransform;
     private Vector2 startDragPosition; 
     private Vector2 lastDragPosition;
+    Vector2 delta;
+    Coroutine inertiaCoroutine;
+    public float inertiaDuration = 3f;
+    [Range (0, 1)] 
+    public float inertiaDampingFactor = 0.5f;
     private bool isDragging;
+    public float dragSpeed = 0.01f;
 
     void Awake()
     {
@@ -23,6 +30,8 @@ public class VillageDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler
         isDragging = false;
         startDragPosition = eventData.position;
         lastDragPosition = eventData.position;
+        if (inertiaCoroutine != null)
+            StopCoroutine(inertiaCoroutine);
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -41,16 +50,34 @@ public class VillageDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler
         if (isDragging)
         {
             // Move "Village" obj (per its rectTransform or whatever) by the pointer position difference
-            Vector2 delta = eventData.position - lastDragPosition;
+            delta = eventData.position - lastDragPosition;
             lastDragPosition = eventData.position;
 
             // Shifts anchor position of that transform
-            rectTransform.anchoredPosition += delta;
+            rectTransform.anchoredPosition += delta * Time.deltaTime * dragSpeed;
         }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag at " + eventData.position);
+        if (isDragging)
+        {
+            inertiaCoroutine = StartCoroutine(ApplyInertia());
+        }
+    }
+
+    private IEnumerator ApplyInertia()
+    {
+        float elapsedTime = 0f;
+        Vector2 currentDelta = delta;
+
+        while (elapsedTime < inertiaDuration)
+        {
+            rectTransform.anchoredPosition += currentDelta * Time.deltaTime * dragSpeed;
+            currentDelta = Vector2.Lerp(currentDelta, Vector2.zero, inertiaDampingFactor);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
