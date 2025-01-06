64250e80a62000b03f229be605b00ae0cf836150
RobertRuta <77187208+RobertRuta@users.noreply.github.com>
2025-01-06 13:42:51 +0400

    Merge conflix fix (#48)

--- Assets/Scripts/VillageModule/VillageController.cs
+++ Assets/Scripts/VillageModule/VillageController.cs
@@ -3,4 +3,13 @@ using UnityEngine;
 public class VillageController : MonoBehaviour
 {
     public float villageDecayRate = 1f;   
+    public float totalVillageHealth = 1f;
+    public float villageHealth;
+    public int buildingCounter = 0;
+
+    void LateUpdate()
+    {
+        villageHealth = totalVillageHealth / (buildingCounter*100f);
+        totalVillageHealth = 0f;
+    }
 }
