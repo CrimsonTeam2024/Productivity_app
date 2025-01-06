95aededa275b7f6dc156f15126d2d2a097ccd4fd
Robert Ruta <robertruta50@gmail.com>
2025-01-06 14:43:35 +0400

    Oracle messages now update based on the oracle satisfaction.

--- Assets/Scripts/GameManagerModule/Oracle.cs
+++ Assets/Scripts/GameManagerModule/Oracle.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Oracle : MonoBehaviour
 {
@@ -7,7 +10,39 @@ public class Oracle : MonoBehaviour
     [SerializeField] TMP_Text tasksCompletedText;
     [SerializeField] TMP_Text focusTimeText;
     [SerializeField] TMP_Text oracleSatisfactionText;
+    [SerializeField] List<Button> oracleMessageButtons;
     int lastTasksCompleted;
+    PopUpMessage popUpMessage;
+
+    List<string>[] messagePools = {
+        new List<string> { 
+            "You are not doing well", 
+            "You need to improve", 
+            "Things are looking bleak", 
+            "This is not good"
+        }, // 0-25% satisfaction
+
+        new List<string> { 
+            "You are doing okay", 
+            "Keep going", 
+            "There's room for improvement", 
+            "You can do better"
+        }, // 26-50% satisfaction
+
+        new List<string> { 
+            "You are doing well", 
+            "Good job", 
+            "Keep up the good work", 
+            "Nice progress"
+        }, // 51-75% satisfaction
+
+        new List<string> { 
+            "You are doing great", 
+            "Excellent work", 
+            "You are on top of things", 
+            "Outstanding performance"
+        } // 76-100% satisfaction
+    };
 
     void Awake()
     {
@@ -17,6 +52,7 @@ public class Oracle : MonoBehaviour
     void Start()
     {
         UpdateOracleStats();
+        UpdateOracleMessages(oracleSatisfaction);
     }
 
     void Update()
@@ -30,5 +66,36 @@ public class Oracle : MonoBehaviour
         uint focusSeconds = GameManager.Instance.focusTime;
         focusTimeText.text = new Timer(focusSeconds).ToString(true);
         oracleSatisfactionText.text = oracleSatisfaction.ToString("P0");
+    }
+
+    public void UpdateOracleMessages(float satisfaction)
+    {
+        List<string> selectedMessagePool;
+
+        if (satisfaction < 0.25f)
+        {
+            selectedMessagePool = messagePools[0];
+        }
+        else if (satisfaction < 0.50f)
+        {
+            selectedMessagePool = messagePools[1];
+        }
+        else if (satisfaction < 0.75f)
+        {
+            selectedMessagePool = messagePools[2];
+        }
+        else
+        {
+            selectedMessagePool = messagePools[3];
+        }
+        int i = 0;
+        foreach (Button msgButton in oracleMessageButtons)
+        {
+            popUpMessage = msgButton.gameObject.GetComponent<PopUpMessage>();
+            string message = selectedMessagePool[i];
+            msgButton.onClick.RemoveAllListeners();
+            msgButton.onClick.AddListener(() => popUpMessage.ShowPopup(message));
+            i++;
+        }
     }
 }
